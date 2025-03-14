ab33caff9f34e703a8fd170fdd467eb227984ec2
Joaquin <jfarfantorres@gmail.com>
2025-03-14 12:26:04 -0300

    Update: Replace navbar logo with dobprotocol-logo.svg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
               aria-label="DOB Protocol - Home"
             >
               <Image 
-                src={theme === 'dark' ? "/dob_imagotipo_dark.svg" : "/dob_imagotipo.svg"} 
+                src="/dobprotocol-logo.svg" 
                 alt="DOB Protocol" 
                 width={180} 
                 height={180} 
